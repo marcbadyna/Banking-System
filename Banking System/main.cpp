31e7fe20da01e5f957acf1223e2ca065598699c4
marcbadyna <marcbadyna@gmail.com>
2024-06-14 14:06:14 -0400

    Initial Commit

---
+++ Banking System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Banking System
+//
+//  Created by Marc Badyna on 6/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
